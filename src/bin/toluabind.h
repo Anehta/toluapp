f57e55098a03fd1248fc89262491be91dbbb7504
punto <punto@5a290d45-b906-0410-82b0-936c2e6a8c8f>
2007-10-26 21:16:02 +0000

    moving trunk
    git-svn-id: svn://svn.berlios.de/toluapp/trunk@280 5a290d45-b906-0410-82b0-936c2e6a8c8f

---
+++ src/bin/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.92 on Fri Sep  7 17:38:03 2007.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
+
